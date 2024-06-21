d02a363866d9a8456f28663a3668a42c8f73d72f
AndyofStateFarm <andrewjb97@yahoo.com>
2024-06-21 16:11:38 -0400

    Configure linear scale for bar widths.

--- script.js
+++ script.js
@@ -8,5 +8,10 @@ const height = (barHeight + margin) * dataset.length;
 
 // Create SVG chart
 const svg = d3.select("#chart")
-    .attr("width", width)
-    .attr("height", height);
+.attr("width", width)
+.attr("height", height);
+
+// Configure linear scale
+const xScale = d3.scaleLinear()
+.domain([0, d3.max(dataset)])
+.range([50, width]);
